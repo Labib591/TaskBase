ba47f0cf103b805c7e29aaa53e52df0bf415490c
Mahir Mohammed Labib <mahi.labib5@gmail.com>
2025-05-23 17:03:41 +0600

    Polished Home Screen

---
+++ src/Components/Success.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import CountUp from 'react-countup';
+
+const Success = () => {
+  return (
+    <div className="text-center my-10 flex flex-col items-center text-[#021100]">
+      <div>
+        <h1 className="text-[2.5rem] font-extrabold">
+          Hire The Best Proffessionals
+        </h1>
+        <p className="font-medium opacity-80">
+          Our platform connects you with verified, experienced people across
+          various specialties — all at your convenience.
+        </p>
+      </div>
+      <div className="md:flex md:flex-row md:gap-4 md:mt-10 flex flex-col gap-4 mt-10">
+        <div className="bg-[#DBF6D7] px-12 py-10 rounded-2xl flex flex-col gap-2 text-left">
+          
+          <h1 className="text-[4rem] font-extrabold"><CountUp duration={4} enableScrollSpy 
+          scrollSpyDelay={200} end={900}/>+</h1>
+          <p className="font-medium opacity-80 text-2xl">Total Tasks Completed</p>
+        </div>
+        <div className="bg-[#DBF6D7] px-12 py-10 rounded-2xl flex flex-col gap-2 text-left">
+         
+          <h1 className="text-[4rem] font-extrabold"><CountUp duration={4} enableScrollSpy 
+          scrollSpyDelay={200} end={467}/>+</h1>
+          <p className="font-medium opacity-80 text-2xl">Total Reviews</p>
+        </div>
+        <div className="bg-[#DBF6D7] px-12 py-10 rounded-2xl flex flex-col gap-2 text-left">
+          
+          <h1 className="text-[4rem] font-extrabold"><CountUp duration={4} enableScrollSpy 
+          scrollSpyDelay={200} end={1000}/>+</h1>
+          <p className="font-medium opacity-80 text-2xl">Freelancers</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Success;
+
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Hero from "../Components/Hero";
 import FeaturedTask from "../Components/FeaturedTask";
+import Success from "../Components/Success";
+import ComponentName from "../Components/Testimonial";
 
 const Home = () => {
     return (
         <div>
             <Hero></Hero>
             <FeaturedTask></FeaturedTask>
+            <Success></Success>
+            <ComponentName></ComponentName>
         </div>
     );
 };
